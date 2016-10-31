85b3cb51732c70e73e444ef319de1f90fe91bef8
CARLOBUSTAMANTE <cbustamante@patprimo.com.co>
2016-10-30 21:04:37 -0500

    Initial commit

---
+++ app/src/main/java/com/example/cbustamante/app2_hola_mundo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.cbustamante.app2_hola_mundo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
